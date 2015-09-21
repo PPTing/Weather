8bc068a4aabc2f8624449428438a5bd4e795a394
郑克建 <PPTing@zhengkejiandeMacBook-Pro.local>
2015-09-21 16:34:30 +0800

    init & design activity_main.xml

---
+++ app/src/main/java/me/ppting/weather/MainActivity.java
@@ -0,0 +1,22 @@
+package me.ppting.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+    }
+}
